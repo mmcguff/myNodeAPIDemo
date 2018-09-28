6f7f4a98002dd56a8ff0716249335b461ef9bb67
Matthew McGuff <matthewmcguff85@gmail.com>
2018-09-28 12:05:57 -0500

    (feature):basic GET request added.

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+
+var jsonDATA = {count: 12, message: 'hey'};
+
+const app = express();
+
+app.get('/', function(req, res){
+    console.log('ran GET / route,');
+    
+    res.sendFile(__dirname + '/index.html', function(err){
+        if(err){
+            res.status(500).send(err);
+        }
+    })
+});
+
+app.get('/data', function(req, res){
+    console.log('ran GET /data route');
+   res.json(jsonDATA);
+})
+
+
+app.listen(3000);
+
---
+++ server.js
@@ -0,0 +1,5 @@
+//TODO: Create a basic server with express
+// that will send back index.html file on a GET request to '/'
+// it should send back the jsonDATA on get to DATA
+
+var jsonDATA = {count: 12, message: 'hey'};
